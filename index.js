145049fbb7c7d9ccf0b05e0e7e14014649d1b06f
WalukaUI <cwaluka@yahoo.com>
2021-03-23 00:28:27 -0700

    adding Js array

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-function
+const TopFetures=[
+    {Name:"Basic",Price: "$15",Data:"3Gb of Data"},
+    {Name:"Starter",Price: "$15",Data:"3Gb of Data"},
+    {Name:"Star",Price: "$15",Data:"3Gb of Data"},
+    {Name:"Family",Price: "$15",Data:"3Gb of Data"},
+    {Name:"Family Plus+",Price: "$15",Data:"3Gb of Data"},
+    {Name:"Unlimited",Price: "$15",Data:"3Gb of Data"}
+]
+
+const Features=document.getElementById("Features");
+
+function allFeatures(){
+    for(let i=0; )
+
+}
+allFeatures();
